a767f42a4d24aded425cdc9fe5be9c9a52b06836
Miro <miro@miro-wilms.de>
2021-05-06 20:38:54 +0200

    added newBookForm incl validation

--- JS-course/library/book-object.js
+++ JS-course/library/book-object.js
@@ -13,8 +13,8 @@ class Book {
   info() {
     console.log(
       `${this.title} by ${this.author}, ${this.pages} pages, ${
-        !this.isRead ? "no read yet" : "already read"
-      }`
+        !this.isRead ? 'no read yet' : 'already read'
+      }`,
     );
   }
 
@@ -22,97 +22,128 @@ class Book {
     console.log(this.author);
   }
 }
-
-//access local storage
-const storeLibrary = (() => {
-  if (!localStorage.getItem("myLibrary")) {
-    // add demo data
-    myLibrary.push(new Book("The lean startup", "Eric Ries", 250, true));
-    myLibrary.push(
-      new Book("The hard thing about hard things", "Ben Horowitz", 300, true)
-    );
-    populateStorage();
-  } else {
-    restoreLibrary();
-  }
-
-  function populateStorage() {
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    restoreLibrary();
-  }
-
-  function restoreLibrary() {
-    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-    displayBooks();
-  }
-
-  return { populateStorage };
-})();
-
-//add new books
-
-const newBookBtn = document.querySelector("#newbook");
-newBookBtn.addEventListener("click", () => {
-  addBookToLibrary();
-});
-
-function addBookToLibrary() {
-  newBook = Object.create(Book);
-  newBook.title = prompt("Which book do you want to add to the library?");
-  newBook.author = prompt("Who wrote it?");
-  newBook.pages = prompt("How many pages?");
-  newBook.isRead = prompt("Did you read it yet?");
+// add new books
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = Object.create(Book);
+  newBook.title = title;
+  newBook.author = author;
+  newBook.pages = pages;
+  newBook.isRead = isRead;
   myLibrary.push(newBook);
 
   // save library in local storage
   storeLibrary.populateStorage();
 }
 
-//display library
+// new book form
+function showNewBookForm() {
+  const newBookBackdrop = document.createElement('div');
+  newBookBackdrop.className = 'backdrop';
+  const newBookDiv = document.createElement('div');
+  newBookDiv.className = 'modal';
+
+  const cancelBtn = document.createElement('span');
+  cancelBtn.className = 'modalCloseBtn material-icons md-18';
+  cancelBtn.textContent = 'close';
+  cancelBtn.addEventListener('click', () => newBookBackdrop.remove());
+  newBookDiv.appendChild(cancelBtn);
+
+  const newBookForm = document.createElement('form');
+  newBookForm.setAttribute('onsubmit', '');
+
+  const inputTitle = document.createElement('input');
+  inputTitle.setAttribute('required', '');
+  inputTitle.setAttribute('placeholder', 'Title');
+  newBookForm.appendChild(inputTitle);
+
+  const inputAuthor = document.createElement('input');
+  inputAuthor.setAttribute('required', '');
+  inputAuthor.setAttribute('placeholder', 'Author');
+  newBookForm.appendChild(inputAuthor);
+
+  const inputPages = document.createElement('input');
+  inputPages.setAttribute('required', '');
+  inputPages.setAttribute('placeholder', 'No. Pages');
+  newBookForm.appendChild(inputPages);
+
+  const inputRead = document.createElement('input');
+  inputRead.setAttribute('type', 'checkbox');
+  newBookForm.appendChild(inputRead);
+
+  const inputReadLabel = document.createElement('label');
+  inputReadLabel.textContent = 'Did you read this one yet?';
+  newBookForm.appendChild(inputReadLabel);
+
+  const submitBtn = document.createElement('button');
+  submitBtn.textContent = 'Submit';
+  submitBtn.addEventListener('click', () => {
+    if (newBookForm.checkValidity()) {
+      addBookToLibrary(
+        inputTitle.value,
+        inputAuthor.value,
+        inputPages.value,
+        inputRead.value,
+      );
+    }
+  });
+  newBookForm.appendChild(submitBtn);
+
+  newBookDiv.appendChild(newBookForm);
+
+  newBookBackdrop.appendChild(newBookDiv);
+  document.body.appendChild(newBookBackdrop);
+}
+
+const newBookBtn = document.querySelector('#newbook');
+newBookBtn.addEventListener('click', () => {
+  showNewBookForm();
+});
+
+// display library
 
 function displayBooks() {
-  //refresh view
+  // refresh view
   Array.prototype.forEach.call(
-    document.querySelectorAll(".wrapper div.box"),
-    function (node) {
+    document.querySelectorAll('.wrapper div.box'),
+    (node) => {
       node.parentNode.removeChild(node);
-    }
+    },
   );
 
   myLibrary.forEach((book) => {
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("box");
-    const propList = document.createElement("ul");
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('box');
+    const propList = document.createElement('ul');
     bookDiv.appendChild(propList);
     for (const key in book) {
       if (Object.hasOwnProperty.call(book, key)) {
         const element = book[key];
-        let propBullet = document.createElement("li");
-        propBullet.textContent = key + ": " + element;
+        const propBullet = document.createElement('li');
+        propBullet.textContent = `${key}: ${element}`;
         propList.appendChild(propBullet);
       }
     }
 
-    //buttons
-    const btnRow = document.createElement("div");
-    btnRow.classList.add("btnrow");
+    // buttons
+    const btnRow = document.createElement('div');
+    btnRow.classList.add('btnrow');
 
-    //remove button
-    const removeBook = document.createElement("button");
-    removeBook.addEventListener("click", () => {
-      if (confirm("really remove " + book.title + "?")) {
+    // remove button
+    const removeBook = document.createElement('button');
+    removeBook.addEventListener('click', () => {
+      if (confirm(`really remove ${book.title}?`)) {
         myLibrary.splice(myLibrary.indexOf(book), 1);
         storeLibrary.populateStorage();
         bookDiv.remove();
       }
     });
-    removeBook.textContent = "❌";
+    removeBook.textContent = '❌';
     btnRow.appendChild(removeBook);
 
-    //toggle isRead button
-    const toggleIsRead = document.createElement("button");
-    toggleIsRead.textContent = book.isRead ? "I didn't read it" : "I read it";
-    toggleIsRead.addEventListener("click", () => {
+    // toggle isRead button
+    const toggleIsRead = document.createElement('button');
+    toggleIsRead.textContent = book.isRead ? "I didn't read it" : 'I read it';
+    toggleIsRead.addEventListener('click', () => {
       book.isRead = !book.isRead;
       storeLibrary.populateStorage();
     });
@@ -120,6 +151,32 @@ function displayBooks() {
 
     bookDiv.appendChild(btnRow);
 
-    document.querySelector(".wrapper").appendChild(bookDiv);
+    document.querySelector('.wrapper').appendChild(bookDiv);
   });
 }
+
+// access local storage
+function restoreLibrary() {
+  myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+  displayBooks();
+}
+
+function populateStorage() {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  restoreLibrary();
+}
+
+const storeLibrary = (() => {
+  if (!localStorage.getItem('myLibrary')) {
+    // add demo data
+    myLibrary.push(new Book('The lean startup', 'Eric Ries', 250, true));
+    myLibrary.push(
+      new Book('The hard thing about hard things', 'Ben Horowitz', 300, true),
+    );
+    populateStorage();
+  } else {
+    restoreLibrary();
+  }
+
+  return { populateStorage };
+})();
