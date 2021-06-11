606d529f41317b79b991fd390d34715552daa323
Miro <miro@miro-wilms.de>
2021-06-11 12:39:15 +0200

    moved App compontent to /compontents

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -6,10 +6,22 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="content">
-        <h1>{this.props.title} - welcome!</h1>
+        <h1>{this.props.title}</h1>
+        <p>
+          Please welcome our newest users:{" "}
+          {this.props.userNames.map(
+            (userName, i) =>
+              userName + (i + 1 !== this.props.userNames.length ? ", " : "!")
+          )}
+        </p>
         <Counter />
         <Time />
       </div>
     );
   }
 }
+
+App.defaultProps = {
+  title: "Welcome!",
+  userNames: ["Max", "Moritz", "Fritz"],
+};
