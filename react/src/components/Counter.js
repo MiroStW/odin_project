a778a1689029449b34eeac058dfd780743e6f149
Miro <miro@miro-wilms.de>
2021-06-21 21:16:19 +0200

    hooks & lifecycle tests

--- react/src/components/Counter.js
+++ react/src/components/Counter.js
@@ -1,14 +1,35 @@
 import React from "react";
 
+const ErrorComponent = () => <div>{props.ignore}</div>;
+
 export default class Counter extends React.Component {
   constructor(props) {
+    console.log("constructor");
     super(props);
 
     this.state = {
       count: 0,
+      seed: 0,
     };
 
     this.increaseCounter = this.increaseCounter.bind(this);
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedStateFromProps");
+    if (props.seed && state.seed !== props.seed) {
+      console.log("getDerivedStateFromProps - new seed");
+      return {
+        seed: props.seed,
+        counter: props.seed,
+      };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    console.log("---------------------");
   }
 
   increaseCounter() {
@@ -17,12 +38,52 @@ export default class Counter extends React.Component {
     }));
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp
+    ) {
+      console.log("shouldComponentUpdate - do NOT render");
+      return false;
+    }
+
+    console.log("shouldComponentUpdate - do render");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate");
+    return null;
+  }
+
   render() {
+    console.log("render", this.state.error);
+    if (this.props.showErrorComponent && this.state.error) {
+      return (
+        <div>We have encountered an error! {this.state.error.message}</div>
+      );
+    }
     return (
       <div className="counter">
         <button onClick={this.increaseCounter}>Click Me!</button>
-        <p>{this.state.count}</p>
+        <p>Counter: {this.state.count}</p>
+        {this.props.showErrorComponent ? <ErrorComponent /> : null}
       </div>
     );
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate");
+    console.log("---------------------");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    console.log("---------------------");
+  }
+
+  componentDidCatch(error, info) {
+    console.log("componentDidCatch");
+    this.setState({ error, info });
   }
 }
---
+++ react/src/components/Hooks.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, Component } from "react";
+
+// export default class Hooks extends Component {
+//   render() {
+//     return <div>"test"</div>;
+//   }
+// }
+
+const Hooks = () => {
+  const [color, setColor] = useState("black");
+
+  useEffect(() => {
+    const changeColor = () => {
+      color === "black" ? setColor("red") : setColor("black");
+    };
+
+    document.addEventListener("click", () => changeColor());
+
+    return () => {
+      document.removeEventListener("click", () => changeColor());
+    };
+  }, [color]);
+
+  return (
+    <div>
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
+    </div>
+  );
+};
+
+export default Hooks;
