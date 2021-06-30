a0a512048453177c399e922cc767d73e4523324e
Miro <miro@miro-wilms.de>
2021-06-30 10:38:41 +0200

    added eslint-react-hooks

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
+  plugins: ["react-hooks"],
   rules: {
     "no-unused-expressions": ["error", { allowTernary: true }],
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
   },
 };
